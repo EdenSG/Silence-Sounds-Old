658c53d235b28baf0ecc38073d10947911431af4
Eden S.-G <edgro2693@gmail.com>
2014-07-31 18:41:21 -0400

    lots of stuff

--- build/js/main.js
+++ build/js/main.js
@@ -1,8 +1,18 @@
 $(function() {
+	var 
     $("#silence-title").load("silence-title.html");
     $("#main-nav").load("main-nav.html");
     $("#menu-button").bind("tap", function(e) {
-    	$(this).toggleClass("shown");
-        $("#main-nav").toggleClass("shown");
+        if ($(this).hasClass('shown')) {
+            $(this).removeClass('shown');
+            $("#main-nav").removeClass("shown");
+            $("#main-nav a").focus(function() {
+                $(this).blur();
+            });
+        } else {
+            $(this).addClass('shown');
+            $("#main-nav").addClass("shown");
+            $("#main-nav a").eq(0).focus();
+        }
     });
 });
